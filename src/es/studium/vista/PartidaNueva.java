17e1d2ff36e7e34b00f3233b2f5f3a6034c0c350
Andresmg89 <andresmoralesgarcia89@gmail.com>
2022-06-15 19:50:14 +0200

    Vista

---
+++ src/es/studium/vista/PartidaNueva.java
@@ -0,0 +1,126 @@
+package es.studium.vista;
+
+import java.awt.Button;
+import java.awt.Choice;
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.TextField;
+
+public class PartidaNueva extends Frame
+{
+	private static final long serialVersionUID = 1L;
+	Dialog pedirNumeroJugadores = new Dialog(this, "BlackJack: Nueva Partida", true);
+	Dialog pedirNombresJugadores = new Dialog(this, "BlackJack: Nueva Partida", true);
+	int numeroJugadores = 0;
+	String[] nombresJugadores = null;
+	Choice choNumeroJugadores = new Choice();
+	Button btnContinuar =new Button("Continuar...");
+	
+	TextField txfNombre1 = new TextField(15);
+	Label lblEtiqueta1 = new Label("Jugador 1:");
+	TextField txfNombre2 = new TextField(15);
+	Label lblEtiqueta2 = new Label("Jugador 2:");
+	TextField txfNombre3 = new TextField(15);
+	Label lblEtiqueta3 = new Label("Jugador 3:");
+	TextField txfNombre4 = new TextField(15);
+	Label lblEtiqueta4 = new Label("Jugador 4:");
+	Button btnComenzarPartida = new Button("Comenzar Partida");
+	
+	public PartidaNueva() 
+	{
+		pedirNumeroJugadores.setBackground(Color.RED);
+		pedirNumeroJugadores.setLayout(new FlowLayout());
+		pedirNumeroJugadores.setSize(240,100);
+		pedirNumeroJugadores.setLocationRelativeTo(null);
+		pedirNumeroJugadores.setResizable(false);
+		
+		choNumeroJugadores.add("Elegir número de jugadores...");
+		choNumeroJugadores.add("2");
+		choNumeroJugadores.add("3");
+		choNumeroJugadores.add("4");
+		pedirNumeroJugadores.add(choNumeroJugadores);
+		pedirNumeroJugadores.add(btnContinuar);
+	}
+	
+	public void MostrarDialogNumeroJugadores() 
+	{
+		pedirNumeroJugadores.setVisible(true);
+	}
+	
+	public void OcultarDialogNumeroJugadores()
+	{
+		pedirNumeroJugadores.setVisible(false);
+	}
+	
+	public void PrepararDialogNombresJugadores(int numero)
+	{
+		pedirNombresJugadores.setBackground(Color.RED);
+		pedirNombresJugadores.setLayout(new FlowLayout());
+		pedirNombresJugadores.setSize(240,100);
+		pedirNombresJugadores.setLocationRelativeTo(null);
+		pedirNombresJugadores.setResizable(false);
+		
+		//1 jugador
+		pedirNombresJugadores.add(lblEtiqueta1);
+		txfNombre1.selectAll();
+		txfNombre1.setText("");
+		pedirNombresJugadores.add(txfNombre1);
+		
+		//2 jugadores
+		pedirNombresJugadores.add(lblEtiqueta2);
+		txfNombre2.selectAll();
+		txfNombre2.setText("");
+		pedirNombresJugadores.add(txfNombre2);
+		
+		//3 jugadores
+		if(numero ==3) 
+		{
+			pedirNombresJugadores.add(lblEtiqueta3);
+			txfNombre3.selectAll();
+			txfNombre3.setText("");
+			pedirNombresJugadores.add(txfNombre3);
+		}
+		else 
+		{
+			pedirNombresJugadores.remove(lblEtiqueta3);
+			txfNombre3.selectAll();
+			txfNombre3.setText("");
+			pedirNombresJugadores.remove(txfNombre3);
+		}
+		
+		//4 jugadores
+		if(numero ==4) 
+		{
+			pedirNombresJugadores.add(lblEtiqueta3);
+			txfNombre3.selectAll();
+			txfNombre3.setText("");
+			pedirNombresJugadores.add(txfNombre3);
+			pedirNombresJugadores.add(lblEtiqueta4);
+			txfNombre4.selectAll();
+			txfNombre4.setText("");
+			pedirNombresJugadores.add(txfNombre4);
+		}
+		else 
+		{
+			pedirNombresJugadores.remove(lblEtiqueta4);
+			txfNombre4.selectAll();
+			txfNombre4.setText("");
+			pedirNombresJugadores.remove(txfNombre4);
+		}
+		pedirNombresJugadores.add(btnComenzarPartida);
+		this.MostrarDialogNombresJugadores();
+	}
+	
+	public void MostrarDialogNombresJugadores()
+	{
+		pedirNombresJugadores.setVisible(true);
+	}
+	
+	public void OcultarDialogNombresJugadores()
+	{
+		pedirNombresJugadores.setVisible(false);
+	}
+}
